a96db0d096b232eccaeb1b61caa0f11b51292b5e
Steve Alves-Blyt <salves.blyt@gmail.com>
2024-11-13 22:05:34 +0100

    feat(tu): add tests on caddy image

---
+++ caddy/integration-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+eval "$(ssh-agent -s)"
+ssh-add --apple-use-keychain
+
+docker image build --ssh default -t lighthouse-test .
+docker run -p 7333:7333 --name lighthouse-test-container --detach lighthouse-test
+
+sleep 1
+
+set +e
+response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:7333)
+
+if [[ $response -eq 200 ]]; then
+    echo "Request successful"
+    EXIT_CODE=0
+else
+    echo "Request failed with status code: $response"
+    EXIT_CODE=1
+fi
+
+set -e
+
+docker stop lighthouse-test-container
+docker rm lighthouse-test-container
+
+exit $EXIT_CODE
